3482e0897714f74026f289e4248d06652158e4dc
congnghia0609 <congnghia0609@gmail.com>
2025-08-04 13:08:49 +0700

    update code OutlineText UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using Unity.VisualScripting;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject playButton; // Gán trong Inspector
     [SerializeField] private GameObject replayButton; // Gán trong Inspector
-    [SerializeField] private GameObject guideText; // Gán trong Inspector
+    [SerializeField] private TMP_Text guideText; // Gán trong Inspector
     [SerializeField] private TMP_Text bestScoreText;
     [SerializeField] private TMP_Text scoreText;
     public bool gameOver = true;
@@ -17,13 +18,45 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        OutlineText();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
 
+    void OutlineText()
+    {
+        if (bestScoreText != null)
+        {
+            // Tạo một bản sao material để không ảnh hưởng đến các TMP khác
+            Material newMatWhite = new Material(bestScoreText.fontSharedMaterial);
+            newMatWhite.EnableKeyword("OUTLINE_ON");
+            newMatWhite.SetColor("_OutlineColor", Color.white);
+            // newMatWhite.SetColor("_OutlineColor", Color.white.WithAlpha(0.9f));
+            newMatWhite.SetFloat("_OutlineWidth", 0.1f);
+
+            // Áp material mới có outline
+            bestScoreText.fontSharedMaterial = newMatWhite;
+            if (scoreText != null)
+            {
+                scoreText.fontSharedMaterial = newMatWhite;
+            }
+        }
+        if (guideText != null)
+        {
+            // Tạo một bản sao material để không ảnh hưởng đến các TMP khác
+            Material newMatBlack = new Material(guideText.fontSharedMaterial);
+            newMatBlack.EnableKeyword("OUTLINE_ON");
+            // newMatBlack.SetColor("_OutlineColor", Color.black);
+            newMatBlack.SetColor("_OutlineColor", Color.black.WithAlpha(0.7f));
+            newMatBlack.SetFloat("_OutlineWidth", 0.2f);
+
+            // Áp material mới có outline
+            guideText.fontSharedMaterial = newMatBlack;
+        }
     }
 
     public void AddScore(int i)
@@ -48,7 +81,8 @@ public class GameManager : MonoBehaviour
         // Ẩn nút chơi lại.
         replayButton.SetActive(false);
         playButton.SetActive(false);
-        guideText.SetActive(false);
+        // guideText.SetActive(false);
+        guideText.enabled = false;
         gameOver = false;
     }
 
@@ -66,7 +100,8 @@ public class GameManager : MonoBehaviour
         // Hiện nút chơi lại
         replayButton.SetActive(true);
         playButton.SetActive(false);
-        guideText.SetActive(false);
+        // guideText.SetActive(false);
+        guideText.enabled = false;
     }
 
     // Gọi khi nhấn nút Replay
